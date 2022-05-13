65813aedf43c813de6322025b938541b4855f819
Ömer Özdemir <omer.ozdemir1@boun.edu.tr>
2022-05-13 23:02:28 +0300

    back-end of student enroll

--- practice-app/practiceapp/learningPlatform/views/student_enroll.py
+++ practice-app/practiceapp/learningPlatform/views/student_enroll.py
@@ -6,7 +6,9 @@ from ..db_utils import run_statement
 from ..guards import guestGuard
 
 
+#@studentGuard
 def student_enroll(req):
-   ##############
-   ## your code ##
-   ##############
+        username=req.session["username"] #Retrieve the username of the logged-in user
+        course_name = req.POST.get("course_name") #Retrieve the name of the course from the html file
+        run_statement(f"INSERT INTO Enrolls VALUES ( '{username}','{course_name}') ") # insert data into DB
+        return render(req,'student.html') # redirect it to the main page.
