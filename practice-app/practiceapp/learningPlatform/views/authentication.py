c9fda556c6e79dcd6549c36fb11703568942984b
acyishere <acyazici@outlook.com>
2022-05-13 13:08:01 +0300

    views created functions must be added.

---
+++ practice-app/practiceapp/learningPlatform/views/authentication.py
@@ -0,0 +1,25 @@
+from asyncio.windows_events import NULL
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.views.decorators.http import require_http_methods
+from db import run_statement
+
+@require_http_methods(["GET"])
+def studentLogin(req):
+    if req.session.get('user'):
+        return HttpResponseRedirect("/")
+
+    error_message=req.GET.get("fail",False)
+    
+    return render(req, 'login.html',{"error_message": error_message, "headerText": "Student", "bottomText": "student", "formPostPath": "loginAsStudent"})
+
+
+@require_http_methods(["GET"])
+def teacherLogin(req):
+    if req.session.get('user'):
+        return HttpResponseRedirect("/")
+
+    error_message=req.GET.get("fail",False)
+    
+    return render(req, 'login.html',{"error_message": error_message, "headerText": "Teacher", "bottomText": "teacher", "formPostPath": "loginAsTeacher"})
+
---
+++ practice-app/practiceapp/learningPlatform/views/student.py
@@ -0,0 +1,14 @@
+from asyncio.windows_events import NULL
+from types import NoneType
+from unittest import result
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from db import run_statement
+from django.views.decorators.http import require_http_methods
+from ..guards import studentGuard
+
+@studentGuard
+@require_http_methods(["GET"])
+def student(req):
+    return render(req, 'student.html',{'name_surname': req.session.get('user').get('name_surname')})
+
