56762fa20519a924844c1f7fe3bb54fc21cfa7b2
mstfatay <ataymstf35@gmail.com>
2022-05-13 19:43:27 +0300

    added login functionality

--- practice-app/practiceapp/learningPlatform/urls.py
+++ practice-app/practiceapp/learningPlatform/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.authentication.login, name="index"),
-    path('student/',views.student.student,name="student")
+    path('student/',views.student.student, name="student"),
+    path('login/query/', views.authentication.loginQuery, name="loginQuery"),
+    path('logout/query/', views.authentication.logoutQuery, name="logoutQuery")
 ]
--- practice-app/practiceapp/learningPlatform/views/authentication.py
+++ practice-app/practiceapp/learningPlatform/views/authentication.py
@@ -11,3 +11,27 @@ def login(req):
     error_message=req.GET.get("fail",False)
     
     return render(req, 'login.html',{"error_message": error_message})
+
+@guestGuard
+@require_http_methods(["POST"])
+def loginQuery(req):
+    username=req.POST.get("username",False)
+    password=req.POST.get("password",False)
+    if username and password:
+        query=f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
+        result=run_statement(query)
+        if result:
+            req.session["user"]={
+                "username":username,
+                "name_surname": result[0][1],
+                "role": "teacher" if result[0][2] else "student"
+            }
+            return HttpResponseRedirect("/")
+        else:
+            return HttpResponseRedirect("/login/?fail=true")
+    else:
+        return HttpResponseRedirect("/login/?fail=true")
+
+def logoutQuery(req):
+    req.session.clear()
+    return HttpResponseRedirect("/")
