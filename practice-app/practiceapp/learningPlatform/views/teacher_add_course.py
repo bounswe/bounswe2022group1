70a4a08d3f8227ad68bca902b56b434e2ee856d0
Ömer Özdemir <omer.ozdemir1@boun.edu.tr>
2022-05-13 21:21:08 +0300

    Update teacher_add_course.py

--- practice-app/practiceapp/learningPlatform/views/teacher_add_course.py
+++ practice-app/practiceapp/learningPlatform/views/teacher_add_course.py
@@ -1,1 +1,12 @@
-# teacher add a new course
+from asyncio.windows_events import NULL
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.views.decorators.http import require_http_methods
+from ..db_utils import run_statement
+from ..guards import guestGuard
+
+
+def teacher_add_course(req):
+   ##############
+   ## your code ##
+   ##############
