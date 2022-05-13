1416ad06b4af74cce020184f6b9ae616f2aa9bd3
Ömer Özdemir <omer.ozdemir1@boun.edu.tr>
2022-05-13 21:20:11 +0300

    Update student_give_rate.py

--- practice-app/practiceapp/learningPlatform/views/student_give_rate.py
+++ practice-app/practiceapp/learningPlatform/views/student_give_rate.py
@@ -1,1 +1,12 @@
-# student will give a rate from 1 to 5 (integer)
+from asyncio.windows_events import NULL
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.views.decorators.http import require_http_methods
+from ..db_utils import run_statement
+from ..guards import guestGuard
+
+
+def student_give_rate(req):
+   ##############
+   ## your code ##
+   ##############
