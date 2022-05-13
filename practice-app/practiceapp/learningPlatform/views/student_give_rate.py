67e2d2707cf0b0cbeb95230793c28e64a688f8eb
Ömer Özdemir <omer.ozdemir1@boun.edu.tr>
2022-05-14 01:34:15 +0300

    Update student_give_rate.py

--- practice-app/practiceapp/learningPlatform/views/student_give_rate.py
+++ practice-app/practiceapp/learningPlatform/views/student_give_rate.py
@@ -7,6 +7,14 @@ from ..guards import guestGuard
 
 
 def student_give_rate(req):
+   return render(req,'student_give_rate.html')
+   ##############
+   ## your code ##
+   ##############
+
+   
+   
+def student_give_rate_entered(req):
    ##############
    ## your code ##
    ##############
