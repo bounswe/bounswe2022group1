f7ac2973361ea0ca04c5f1f09902ebaa57687cb0
Ömer Özdemir <omer.ozdemir1@boun.edu.tr>
2022-05-15 01:11:07 +0300

    Osman Fehmi Albayrak

--- practice-app/practiceapp/learningPlatform/views/student_my_courses.py
+++ practice-app/practiceapp/learningPlatform/views/student_my_courses.py
@@ -5,6 +5,10 @@ from django.views.decorators.http import require_http_methods
 from ..db_utils import run_statement
 from ..guards import guestGuard
 
+##################
+### Osman Fehmi Albayrak ###
+##################
+
 
 def student_my_courses(req):
    ##############
