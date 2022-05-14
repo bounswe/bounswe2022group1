57a107af71ec0d7780a07a9a119d00ee81a65f2a
acyishere <acyazici@outlook.com>
2022-05-14 11:24:31 +0300

    teacher add courses not final

--- practice-app/practiceapp/learningPlatform/views/teacher_add_course.py
+++ practice-app/practiceapp/learningPlatform/views/teacher_add_course.py
@@ -5,7 +5,7 @@ from django.views.decorators.http import require_http_methods
 from ..db_utils import run_statement
 from ..guards import guestGuard
 
-
+#Ahmet Cemil YAZICI - Teacher adds courses to system.
 
 def teacher_add_course(req):
    return render(req,'teacher_add_course.html')
