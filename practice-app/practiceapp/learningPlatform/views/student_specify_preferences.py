62432ec805ebf6de14a1f6cb39d44d562ff6407d
Ege Onur Taga <42147612+egetaga@users.noreply.github.com>
2022-05-19 00:35:42 +0300

    Ege-API, Front-End, Back-End
    I have finished the API, front-end and back-end parts of my code, which is mainlu about specifying preferences.

--- practice-app/practiceapp/learningPlatform/views/student_specify_preferences.py
+++ practice-app/practiceapp/learningPlatform/views/student_specify_preferences.py
@@ -15,9 +15,12 @@ from django.views.decorators.csrf import csrf_exempt
 ##################
 
 # student_preferences_get, student_preferences_post
-
+@studentGuard
+@require_http_methods(["GET"])
 def student_specify_preferences(req):
-   
+   success =  True if req.GET.get('success', 'true')=='true' else False
+   error = req.GET.get("error","")
+
    my_req = HttpRequest()
    my_req.method = "GET"
    student_username = req.session.get("user", dict()).get("username", "")
@@ -27,14 +30,56 @@ def student_specify_preferences(req):
 
    if student_preferences_response_object.status_code == 200:
       preferences_content = json.loads(student_preferences_response_object.content)
-      return render(req,"student_specify_preferences.html", {"preferences_content": preferences_content.get("preference_list", []), "success": True } )
+      values = preferences_content.values()
+      preferences_list = []
+      for value in values:
+         topic = value['topic']
+         response = requests.get(f'https://en.wikipedia.org/w/rest.php/v1/search/title?q={topic}&limit=1')
+         content = json.loads(response.content)
+         content_pages = content['pages']
+         if len(content_pages) == 0:
+            preferences_list.append([value['topic'], value['level'], 'There is no entry in wikipedia for this topic.' ])
+         else:
+            preferences_list.append([value['topic'], value['level'], content_pages[0].get('description', 'There is no entry in wikipedia for this topic.')])
 
+      return render(req,"student_specify_preferences.html", {"preferences_content": preferences_list, "success": True, "error":error } )
 
-   return render(req,'student_specify_preferences.html', {"success": False } )
+   return render(req,'student_specify_preferences.html', {"success": False, "error":error } )
 
-
+@studentGuard
+@require_http_methods(["POST"])
 def student_specify_preferences_entered(req):
-   pass
+   topic = req.POST.get('topic', False)
+   level = req.POST.get('level', False)
+   student_username = req.session.get("user", dict()).get("username", "")
+   password = ''
+
+   try:
+      query=f"SELECT password FROM users WHERE username='{student_username}' AND is_teacher=False;"
+      return_result = run_statement(query)
+      password = return_result[0][0]
+   except:
+      return HttpResponseRedirect('/student/student_specify_preferences?success=false&error='+ 'There is no user with given username and password.' )
+
+
+   if level != 'Beginner' and level != 'Intermediate' and level != 'Advanced':
+      return HttpResponseRedirect('/student/student_specify_preferences?success=false&error='+'The level should be Beginner, Intermediate or Advanced. Case Sensitive.' )
+    
+   cust_req = HttpRequest()
+   cust_req.method = "POST"
+   cust_req.POST = {'student_username': student_username, 'password': password, 'topic': topic, 'level':level}
+   response_obj = student_preferences_post(cust_req)
+   
+   if response_obj.status_code == 200:
+      content = json.loads(response_obj.content)
+      status = content.get('status', 'true')
+      error = content.get('error', '')
+      return HttpResponseRedirect(f'/student/student_specify_preferences?success={status}&error={error}')
+   
+   return HttpResponseRedirect('/student/student_specify_preferences?success=fail&error=' + 'Cannot update the preferences.')
+
+   
+   
    ##############
    ## your code ##
    ##############
@@ -42,27 +87,51 @@ def student_specify_preferences_entered(req):
 @csrf_exempt
 @require_http_methods(["GET"])
 def student_preferences_get(req):
-   student_username = req.GET.get("student_username", "")
-   query = f"SELECT * FROM Preferences WHERE student_username='{student_username}'"
+   student_username = req.GET.get('student_username', "")
+   query = f"SELECT Preferences.topic, Preferences.level FROM Preferences WHERE student_username='{student_username}';"
    result = run_statement(query)
-   modified_result = []
-   for res in result:
-      modified_result.append([res[1], res[2]])
+   modified_result = dict()
+   for i in range(len(result)):
+      preference_name = f'preference_{(i+1)}'
+      modified_result[preference_name] = {'topic':result[i][0], 'level':result[i][1]}
 
-
-   final_result = {"preference_list":modified_result}
-   return JsonResponse(final_result)
+   j_response = JsonResponse(modified_result)
+   return j_response
 
 @csrf_exempt
 @require_http_methods(["POST"])
 def student_preferences_post(req):
-   student_username = req.POST['student_username']
-   topic = req.POST['topic']
-   level = req.POST['level']
-   query = f"INSERT INTO Preferences VALUES('{student_username}', '{topic}','{level}');"
-   result = run_statement(query)
-   response = {"student_username":student_username, "topic":topic, "level":level }
+   student_username = req.POST.get('student_username', False)
+   password = req.POST.get('password', False)
+   topic = req.POST.get('topic', False)
+   level = req.POST.get('level', False)
+
+   if student_username == False or password == False or topic == False or level == False:
+      return JsonResponse({'status': 'false', 'error': 'Please specify student_username, password, topic and level.'})
+
+   try:
+      query=f"SELECT * FROM users WHERE username='{student_username}' AND password='{password}' AND is_teacher=False;"
+      return_result = run_statement(query)
+      if len(return_result) == 0:
+         return JsonResponse({'status': 'false', "error":"There is no student with given username and password." })
+   except:
+      return JsonResponse({'status': 'false', "error":"An error occurred while searching the query in the database" })
+
+   if level != 'Beginner' and level != 'Intermediate' and level != 'Advanced':
+      return JsonResponse({'status': 'false', "error":"The level should be Beginner, Intermediate or Advanced. Case Sensitive." })
+
+
+
+   response = dict()
+   try:
+      query = f"INSERT INTO Preferences VALUES('{student_username}', '{topic}','{level}');"
+      run_statement(query)
+      response = {'status':'true',  'student_username':student_username, 'topic':topic, 'level':level }
+   except:
+      return JsonResponse({'status': 'false', "error":"An error occurred while performing the query. It maybe because non-unique student_username-topic pair. " })
+   
    return JsonResponse(response)
+
 
 
 
