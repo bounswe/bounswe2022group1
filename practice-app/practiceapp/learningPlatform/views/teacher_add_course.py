41f5346c1ba866961f89572472e6041821905810
acyishere <acyazici@outlook.com>
2022-05-18 21:18:50 +0300

    finalized teacher_add_course

--- practice-app/practiceapp/learningPlatform/views/teacher_add_course.py
+++ practice-app/practiceapp/learningPlatform/views/teacher_add_course.py
@@ -3,18 +3,40 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.views.decorators.http import require_http_methods
 from ..db_utils import run_statement
 from ..guards import guestGuard, teacherGuard
+import http.client
+import json
+import requests
+import random
 
-##################
-### Ahmet Cemil Yazıcı ###
-##################
 #Ahmet Cemil YAZICI - Teacher adds courses to system.
+
+# EXTERNAL API
+conn = http.client.HTTPSConnection("quotes15.p.rapidapi.com")
+
+headers = {
+    'X-RapidAPI-Host': "quotes15.p.rapidapi.com",
+    'X-RapidAPI-Key': "14656f2ef3msh7f73a5ceb897daap14ce31jsna93569cb63f0"
+    }
+
+def callExternalAPI():
+
+   conn.request("GET", "/quotes/random/", headers=headers)
+   res = conn.getresponse()
+   data = res.read()
+
+   if "content" in json.loads(data.decode("utf-8")):
+      return json.loads(data.decode("utf-8"))["content"]
+   else:
+      return "You are trying to generate too often, please retry in a few seconds."
 
 @teacherGuard
 @require_http_methods(["POST","GET"])
 def teacher_add_course(req):
+
+   random_sentence_generated = callExternalAPI()
    success = req.GET.get('success', False)
    fail = req.GET.get('fail', False)
-   return render(req,'teacher_add_course.html',{'success': success, 'fail': fail})
+   return render(req,'teacher_add_course.html',{'success': success, 'fail': fail, 'random_sentence_generated': random_sentence_generated})
 
 @teacherGuard
 @require_http_methods(["POST","GET"])
@@ -35,8 +57,7 @@ def teacher_add_course_entered(req):
    except:
       return HttpResponseRedirect("/teacher/teacher_add_course/?fail=true")
 
-   # External API
-   
+
 
 
 
