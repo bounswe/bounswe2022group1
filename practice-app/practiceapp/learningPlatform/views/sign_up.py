f29df32977f992f2ec711ad91cb17589d78f1573
quanex1 <omer.ozdemir1@boun.edu.tr>
2022-05-19 22:16:07 +0300

    unit_test structured

--- practice-app/practiceapp/learningPlatform/views/sign_up.py
+++ practice-app/practiceapp/learningPlatform/views/sign_up.py
@@ -56,40 +56,31 @@ def call_external_API():
 
 @csrf_exempt
 def can_sign_up(req):
-    username=req.GET.get("username",False) #get username
+    username=req.POST.get("post_username",False) #get username
     if username:
         if run_statement(f"SELECT username FROM {dbname}.Users WHERE '{username}'=username "): # username already exists
-            return JsonResponse({'can_sign_up': False,'error_reason':"The entered username already exists!"})
+            return False
         else: # username does not exist in database
-            return JsonResponse({'can_sign_up': True})
+            return True
     else: # name_surname or username or password not given
-        return JsonResponse({'can_sign_up': False, 'error_reason':"Username must be given!"})
+        return False
     
 @csrf_exempt
 @require_http_methods(["POST"])
 def do_sign_up(req):
-    name_surname=req.POST.get("name_surname",False)#get name-surname
-    username=req.POST.get("username",False) #get username
-    password=req.POST.get("password",False) #get password
-    is_teacher=req.POST.get('is_teacher',False) # get is_teacher
-    
-    my_req = HttpRequest()
-    my_req.method = "GET"
-    my_req.GET = {'username': username, 'name_surname':name_surname,'password': password,'is_teacher':is_teacher}
-    can_sign_up_response_object = can_sign_up(my_req) # we check whether with given parameters, user can signup
-    response_content = json.loads(can_sign_up_response_object.content) # json is converted to python dictionary
+    name_surname=req.POST.get("post_name_surname",False)#get name-surname
+    username=req.POST.get("post_username",False) #get username
+    password=req.POST.get("post_password",False) #get password
+    is_teacher=req.POST.get('post_is_teacher',False) # get is_teacher
 
-    if can_sign_up_response_object.status_code == 200:
-        if response_content['can_sign_up']:
+    if can_sign_up(req).status_code == 200:
             try:
                 run_statement(f"INSERT INTO {dbname}.Users (username, name_surname, is_teacher, password) VALUES('{username}', '{name_surname}', {is_teacher}, '{password}' ) ;") #insert into Users table
-                return JsonResponse({'isSignedUp': True})
+                return True
             except: #due to html protocols, insertion could not be done.
-                return JsonResponse({'isSignedUp': False, 'error_reason': 'An error occured due to HTML protocols.'})
-        else: # can_sign_up failed! Either username exists or username was not given
-            return JsonResponse({'isSignedUp': False, 'error_reason': response_content.get('error_reason',"Reason of the error is not known!")})
+                return False
     else:  # can_sign_up failed due to html protocols
-        return JsonResponse({'isSignedUp': False, 'error_reason': response_content.get('error_reason',"Reason of the error is not known!")})
+        return False
     
     
 @csrf_exempt
@@ -106,31 +97,38 @@ def sign_up_entered(req):
     password=req.POST.get("post_password") #get password
     is_teacher=req.POST.get('post_is_teacher',False) # get is_teacher
     
-    my_req = HttpRequest()
-    my_req.method = "POST"
-    my_req.POST = {'username': username, 'name_surname':name_surname,'password': password,'is_teacher':is_teacher}
-    do_sign_up_response_object = do_sign_up(my_req)
-    
-    if do_sign_up_response_object.status_code==200: # signed succeeds
+    if do_sign_up(req).status_code==200: # signed_up succeeds
         return HttpResponseRedirect("/login/")
-    else: # signed fails
+    else: # signed_up fails
         return render(req,'sign_up.html',{'is_first_time':False,'fail':True,'check_username_not_available':check_username_not_available,'responseOfAPI':responseOfAPI})
 
+
+
+
+def sign_up_does_check_username_exist(req):
+    check_username=req.GET.get("check_username")
+    return run_statement(f"SELECT username FROM {dbname}.Users WHERE '{check_username}'=username ")
     
 @csrf_exempt
 def sign_up_check_username(req):
     responseOfAPI=call_external_API()
     check_username=req.POST.get('post_check_username',"")
-    check_username_not_available=run_statement(f"SELECT username FROM {dbname}.Users WHERE '{check_username}'=username ")
-    return render(req,'sign_up.html',{'is_first_time':False,'fail':False,'check_username_not_available':check_username_not_available,'responseOfAPI':responseOfAPI})
+    
+    my_req = HttpRequest()
+    my_req.method = "GET"
+    my_req.GET = {'check_username': check_username}
+    if sign_up_does_check_username_exist(my_req):
+        return render(req,'sign_up.html',{'is_first_time':False,'fail':False,'check_username_not_available':False,'responseOfAPI':responseOfAPI})
+    else:
+        return render(req,'sign_up.html',{'is_first_time':False,'fail':False,'check_username_not_available':True,'responseOfAPI':responseOfAPI})
 
 
+def get_see_all_registered_users(req):
+    return run_statement(f"SELECT username FROM {dbname}.Users")
 
 # http://127.0.0.1:8000/see_all_registered_users/
-from django.http import JsonResponse
 def see_all_registered_users_method(req):
-    query=run_statement(f"SELECT username FROM {dbname}.Users")
-    return JsonResponse({'RegisteredUsers':query})
+    return JsonResponse({'RegisteredUsers':get_see_all_registered_users(req)})
 
 
 
