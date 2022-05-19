fb36031497b96b608d9c35c317526a21a2e6db62
ecesrkn <ecesarkin@gmail.com>
2022-05-19 17:23:23 +0300

    student_enroll method

--- practice-app/practiceapp/learningPlatform/views/student_enroll.py
+++ practice-app/practiceapp/learningPlatform/views/student_enroll.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.views.decorators.http import require_http_methods
@@ -8,11 +9,34 @@ from ..guards import studentGuard
 ### Ece Sarkın ###
 ##################
 
+# External API
+
 
 @studentGuard
+@require_http_methods(["GET"])
 def student_enroll(req):
-        return render(req,'student_enroll.html')
+        username = req.session["user"]["username"]
+        result = run_statement(f"SELECT course_name FROM Enrolls WHERE student_username = '{username}' ")
 
+        response = requests.get("https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/single").json()
+
+
+        #print(response.text)
+        #return render(req,'student_enroll.html')
+
+
+        finalResult = []
+        for i in result:
+            finalResult.append(f"{i+1}.) ")
+            finalResult.append(i[0])
+
+        return render(req, 'student_enroll.html', {"quotes_list": finalResult})
+
+
+## return button
+@studentGuard
+def student_enroll_back(req):
+   return HttpResponseRedirect("/student")
 
 def student_enroll_entered(req):
         username=req.session["username"] #Retrieve the username of the logged-in user
