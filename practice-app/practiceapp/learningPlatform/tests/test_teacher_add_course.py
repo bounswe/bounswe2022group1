22c7c3627fe8e26d5a9a7bbae44fe71d4ae7466f
acyishere <acyazici@outlook.com>
2022-05-20 20:30:52 +0300

    LAST CHANGES OVER TEACHER ADD COURSE

--- practice-app/practiceapp/learningPlatform/tests/test_teacher_add_course.py
+++ practice-app/practiceapp/learningPlatform/tests/test_teacher_add_course.py
@@ -5,10 +5,17 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.db import connection
 
+from ..db_utils import run_statement
+import environ
+env = environ.Env()
+environ.Env.read_env()
+dbname=env("MYSQL_DATABASE")
+
 
 class test_teacher_add_course(TestCase):
 
     def test_valid_course_add(self):
+        run_statement(f"USE {dbname};")
         my_req = HttpRequest()
         my_req.method = "POST"
         
@@ -19,6 +26,7 @@ class test_teacher_add_course(TestCase):
         self.assertTrue(teacher_add_course.can_add_course(my_req))
 
     def test_non_valid_course_add(self):
+        run_statement(f"USE {dbname};")
         my_req = HttpRequest()
         my_req.method = "POST"
         
