e833cc5bc48558b3ab470ce80e04f882d1e3c263
Ömer Özdemir <omer.ozdemir1@boun.edu.tr>
2022-10-29 22:54:53 +0300

    sign_up update

--- android/app/src/main/java/com/example/myapplication/SignUpActivity.kt
+++ android/app/src/main/java/com/example/myapplication/SignUpActivity.kt
@@ -1,9 +1,15 @@
-package com.example.myapplication
+package com.example.myapplication.view
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
+import com.example.myapplication.R
+import com.example.myapplication.model.sign_up_model
+import com.example.myapplication.service.RestApiService
+
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +17,24 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
     }
 
-    fun go_to_login_page(view: View) {
+    fun goToLoginPage(view: View) {
         var intent= Intent(applicationContext, SignInActivity::class.java)
         startActivity(intent)
     }
+
+     fun toSignUp(view: View){
+        val apiService = RestApiService()
+        val userInfo = sign_up_model(
+            username = "quanex1",
+            email = "alex@gmail.com",
+            password = "asd123asd123")
+
+        apiService.addUser(userInfo) {
+
+            val success_message=findViewById(R.id.success_message) as TextView
+            success_message.text=it?.toString()
+            println(it?.toString())
+        }
+    }
+
 }
