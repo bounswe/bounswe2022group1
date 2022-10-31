28abb2afa575ea315d4b7da9c5c1c6659976128b
omerozdemir1 <omer.ozdemir1@boun.edu.tr>
2022-10-31 18:37:29 +0300

    forgot password interface is introduced.

---
+++ android/app/src/main/java/com/example/myapplication/view/ForgotPassword.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgotPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
--- android/app/src/main/java/com/example/myapplication/view/HomeActivity.kt
+++ android/app/src/main/java/com/example/myapplication/view/HomeActivity.kt
@@ -9,6 +9,5 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        //println("I am home"+user_token)
     }
 }
