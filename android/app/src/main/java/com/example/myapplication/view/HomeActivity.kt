bdb831d689dd645b504a77a771e5748581aa0c04
Osman Albayrak <gecemavisioa@hotmail.com>
2022-11-20 18:06:57 +0300

    Left Navigation Bar implemented to the Home Page

--- android/app/src/main/java/com/example/myapplication/view/HomeActivity.kt
+++ android/app/src/main/java/com/example/myapplication/view/HomeActivity.kt
@@ -4,18 +4,45 @@ package com.example.myapplication.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.myapplication.R
+import com.google.android.material.navigation.NavigationView
 import java.nio.file.attribute.AttributeView
 
 class HomeActivity : AppCompatActivity() {
 
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val string = findViewById<DrawerLayout>(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, string, R.string.open, R.string.close)
+
+        string.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val string2 = findViewById<NavigationView>(R.id.navView)
+
+        string2.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.miItem1 -> Toast.makeText(applicationContext, "Clicked Item 1", Toast.LENGTH_SHORT).show()
+                R.id.miItem2 -> Toast.makeText(applicationContext, "Clicked Item 2", Toast.LENGTH_SHORT).show()
+                R.id.miItem3 -> Toast.makeText(applicationContext, "Clicked Item 3", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
         //println("I am home"+user_token)
         //user token null değil ise buttonları göster log off göster sign in kapat
         // , null ise sign in göster, butonları kapat logoff kapat
@@ -44,6 +71,14 @@ class HomeActivity : AppCompatActivity() {
             toLanding()
         }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     fun to_signIn() {
         var intent= Intent(applicationContext, SignInActivity::class.java)
         startActivity(intent)
