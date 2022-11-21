53fc8be36017c0a1b9c4e67026cf51c4e1728fb8
omerozdemir1 <omer.ozdemir1@boun.edu.tr>
2022-11-21 10:36:41 +0300

    review+design of 2 and 3 phases

---
+++ android/app/src/main/java/com/example/myapplication/view/LearningSpace2.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LearningSpace2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learning_space2)
+    }
+}
