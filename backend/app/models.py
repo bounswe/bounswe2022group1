81c0759fda50326a11ec7844efd3569aac64e3f1
mstfatay <ataymstf35@gmail.com>
2022-11-22 02:19:04 +0300

    update LearningSpace and Content models

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,10 +4,17 @@ from django.contrib.auth.models import User
 
 class LearningSpace(models.Model):
     name = models.CharField(max_length=30)
-    #contributors = models.ManyToManyField(User, related_name='contributors')
+    members = models.ManyToManyField(User, related_name='members')
+
+    # TODO: add contributors field
+    # I think contributors shouldn't be a field in the model, but rather a method
+    # because contributers can be found by owners of content_list elements
+    # def contributors(self):
+    #     return self.content_list.all().values_list('owner', flat=True)
+        
 
     # implicitly existing fields:
-    # contents
+    # content_list
 
     # TODO: add chat
     # chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat')
@@ -16,9 +23,11 @@ class LearningSpace(models.Model):
 
 class Content(models.Model):
     name = models.CharField(max_length=30)
-    type = models.CharField(max_length=30)
     text = models.TextField()
     url = models.TextField()
+
+    # type can be either text, video, image, discussion, or meeting
+    type = models.CharField(max_length=30)
 
     # TODO: how does image uploading works? (it is not saved in the database)
     # image = models.ImageField(upload_to='images/')
@@ -27,7 +36,7 @@ class Content(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
     learningSpace = models.ForeignKey(LearningSpace, on_delete=models.CASCADE)
 
-    votes = models.IntegerField(default=0)
+    upVoteCount = models.IntegerField(default=0)
     
     # TODO: add chat
     # chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat')
