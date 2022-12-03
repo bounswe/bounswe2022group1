6b98e64a6a44635a863201ec8e2c07d5ff7fcbb5
Ege Onur Taga <42147612+egetaga@users.noreply.github.com>
2022-12-03 18:26:35 +0300

    Implemented an APIs searching and listing all LS

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,7 +3,7 @@ from .views import Login, discussionApiView, discussionApiListView
 from django.urls import path
 from knox import views as knox_views
 from django.urls import path
-from .views import ChangePassword,LearningSpaceApiView
+from .views import ChangePassword,LearningSpaceApiView, LearningSpaceListApiView, LearningSpaceSearchApiView
 
 
 urlpatterns = [
@@ -18,6 +18,9 @@ urlpatterns = [
     path('app/enroll/', enrollApiView.as_view(), name='enroll'),
     path('app/discussion/', discussionApiView.as_view(), name='discussion'),
     path('app/discussion-list/', discussionApiListView.as_view(), name='discussion-list'),
+    path('app/learning-space-list/', LearningSpaceListApiView.as_view(), name='learning-space-list'),
+    path('app/learning-space-search/', LearningSpaceSearchApiView.as_view(), name='learning-space-search'),
+
 
 
 ]
