b2bf5d28ff35285b91f467f02597fedb527614ec
mstfatay <ataymstf35@gmail.com>
2022-12-04 21:40:57 +0300

    add image storing feature

--- backend/budemi/urls.py
+++ backend/budemi/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
- ]
+]
+
+# images are stored here
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
