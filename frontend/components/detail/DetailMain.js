806eba71915ace4896a93823b8ef5700b115b435
huseyin.seyyid <huseyin.seyyid@artiwise.com>
2022-12-16 15:06:57 +0300

    A function added for leave action from LS

--- frontend/components/detail/DetailMain.js
+++ frontend/components/detail/DetailMain.js
@@ -28,46 +28,35 @@ const handleJoin = (spid) => {
    });
 }
 
-function DetailMain({ space }) {
-  return (
-    <Grid>
+const handleLeave = (spid) => {
+	axios
+		.post(
+			`http://3.89.218.253:8000/app/leave-learning-space/`,
+			{
+				learning_space_id: spid,
+			},
+			{
+				headers: {
+					Authorization: `token ${localStorage.getItem('token')}`,
+				},
+			}
+		)
+		.then(
+			(response) => {
+				console.log(response.data);
+				alert(
+					'Successfully left ' +
+						response.data.name +
+						' for user ' +
+						localStorage.getItem('user')
+				);
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+};
 
-      <Box component="form"
-            noValidate
-            sx={{ mt: 1 }}>
-        {
-          <Card sx={{ mt: 2 }}>
-            <CardHeader
-              
-              title={space?.name+" Learning Space"}
-              style={{ textAlign: 'center'}}
-              titleTypographyProps={{variant:'h2' }}
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-
-                
-                <Box display="flex" justifyContent="space-between">
-                  <FormControl>
-                  <InputLabel id="demo-simple-select-label">Add Content</InputLabel>
-                    <Select
-                      labelId="addcontent-label"
-                      id="addcontent-select"
-                      value="addcontent"
-                      label="addcontent"
-                      displayEmpty
-                      sx={{ width: 200}}
-                    >
-                      <MenuItem value="">Add Content</MenuItem>
-                      <MenuItem component={Link} href={`/addcontent/${space?.id}/text`}>Text</MenuItem>
-                      <MenuItem component={Link} href={`/addcontent/${space?.id}/video`}>Video</MenuItem>
-                      <MenuItem component={Link} href={`/addcontent/${space?.id}/image`}>Image</MenuItem>
-                      <MenuItem component={Link} href={`/addcontent/${space?.id}/meeting`}>Meeting</MenuItem>
-                      <MenuItem component={Link} href={`/addcontent/${space?.id}/discussion`}>Discussion</MenuItem>
-                    </Select>
-                  </FormControl>
-                  <Button type="submit" onClick={() => handleJoin(space?.id)} variant="contained" sx={{ mt: 3, mb: 2, borderRadius: "16px" }} className="btn btn-primary">Join</Button>
-                </Box>
 
 
               </Typography>
