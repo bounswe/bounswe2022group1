b469b2aa649e7f6763e6e3c0c8638033c04102e5
omerozdemir1 <omer.ozdemir1@boun.edu.tr>
2022-12-16 07:40:50 +0300

    LearningSpace2Members functionality change. Not only content owners but also enrolled users are members.

--- android/app/src/main/java/com/example/myapplication/view/LearningSpace2Members.kt
+++ android/app/src/main/java/com/example/myapplication/view/LearningSpace2Members.kt
@@ -2,11 +2,13 @@ package com.example.myapplication.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import com.example.myapplication.R
 import com.example.myapplication.service.learningSpace2GetContentList_api_call
+import com.example.myapplication.service.learningSpace2ListEveryLearningSpace_api_call
 
 class LearningSpace2Members : AppCompatActivity() {
 
@@ -19,41 +21,38 @@ class LearningSpace2Members : AppCompatActivity() {
         var learning_topic = findViewById(R.id.MemberText) as TextView
         learning_topic.text= learningSpaceNAME+" Members"
 
+        val apiService = learningSpace2ListEveryLearningSpace_api_call()
+        apiService.listEverySpace () {
+            if (it?.data != null) {
+                for (i in 0..(it.data.size - 1)) {
 
-        ShowContributorsAndTopics()
+                    if (it.data[i].id == learningSpaceID) {
+                        contributors= arrayOf<String>()
+
+                        for(j in 0..(it.data[i].members.size-1)){
+                            contributors+=it.data[i].members[j].name
+                        }
+                        setContributorsAndTopics()
+                        break
+                    }
+                }
+            }
+            else{ // error for API
+
+            }
+        }
+
     }
 
     fun setContributorsAndTopics(){
         val contributorsListView = findViewById<ListView>(R.id.Owners)
 
         var contributorsAdapter: ArrayAdapter<String> = ArrayAdapter(
-            this, com.example.myapplication.R.layout.adapter_background, contributors
+            this, com.example.myapplication.R.layout.adapter_background,contributors
         )
 
         contributorsListView.adapter=contributorsAdapter
+
     }
-
-    fun ShowContributorsAndTopics(){
-        val apiService = learningSpace2GetContentList_api_call()
-        val userInfo = learningSpaceID
-
-        apiService.getContentList(userInfo) {
-
-            if(it?.data!=null){ // success
-                var receivedArr=it?.data
-                contributors= arrayOf<String>()
-                for (i in 0..(learningSpaceMEMBERS.size-1)){
-                    contributors+= learningSpaceMEMBERS[i].name
-                }
-
-                setContributorsAndTopics()
-            }
-            else{ // showing contributors is unsucess
-
-            }
-        }
-    }
-
-
 
 }
