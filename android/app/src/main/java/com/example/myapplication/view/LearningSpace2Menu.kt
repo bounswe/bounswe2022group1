32205b19709f6a3eb1959517f3151589975036d8
omerozdemir1 <omer.ozdemir1@boun.edu.tr>
2022-12-16 07:22:24 +0300

    LearningSpace2Menu leave and enroll functionality is implemented.

--- android/app/src/main/java/com/example/myapplication/view/LearningSpace2Menu.kt
+++ android/app/src/main/java/com/example/myapplication/view/LearningSpace2Menu.kt
@@ -15,21 +15,23 @@ import com.example.myapplication.service.learningSpace2GetEnrolledLearningSpaces
 import com.example.myapplication.service.learningSpace2Leave_api_call
 
 class LearningSpace2Menu : AppCompatActivity() {
-    var resource_button=Button(this)
-    var resource_image=ImageView(this)
+    private lateinit var resource_button:Button
+    private lateinit var resource_image:ImageView
 
-    var member_button=Button(this)
-    var member_image=ImageView(this)
+    private lateinit var member_button:Button
+    private lateinit var member_image:ImageView
 
 
-    var addresource_button=Button(this)
-    var addresource_image=ImageView(this)
+    private lateinit var addresource_button:Button
+    private lateinit var addresource_image:ImageView
 
-    var addfavorite_button=Button(this)
-    var addfavorite_image=ImageView(this)
+    private lateinit var addfavorite_button:Button
+    private lateinit var addfavorite_image:ImageView
 
-    var enroll_button=Button(this)
-    var enroll_image=ImageView(this)
+    private lateinit var enroll_button:Button
+    private lateinit var enroll_image:ImageView
+
+    private var isMember:Boolean=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,9 +50,28 @@ class LearningSpace2Menu : AppCompatActivity() {
         addfavorite_image = findViewById(R.id.addfavorite_image) as ImageView
 
         enroll_button = findViewById(R.id.enroll_button) as Button
+        enroll_image=findViewById(R.id.enroll_image) as ImageView
 
         var learning_topic = findViewById(R.id.topic) as TextView
         learning_topic.text= learningSpaceNAME
+
+        resource_image.setVisibility(View.GONE)
+        resource_button.setVisibility(View.GONE)
+
+        member_button.setVisibility(View.GONE)
+        member_image.setVisibility(View.GONE)
+
+        addresource_button.setVisibility(View.GONE)
+        addresource_image.setVisibility(View.GONE)
+
+        addfavorite_button.setVisibility(View.GONE)
+        addfavorite_image.setVisibility(View.GONE)
+
+        enroll_image.setImageResource(R.drawable.addmember)
+
+        enroll_button.text = "Enroll"
+
+        initScreen()
     }
 
     fun goToResources(view: View) {
@@ -72,9 +93,72 @@ class LearningSpace2Menu : AppCompatActivity() {
 
     }
 
+
+    fun updateScreen(isMember:Boolean){
+
+        if(isMember){
+            resource_image.setVisibility(View.VISIBLE)
+            resource_button.setVisibility(View.VISIBLE)
+
+            member_button.setVisibility(View.VISIBLE)
+            member_image.setVisibility(View.VISIBLE)
+
+            addresource_button.setVisibility(View.VISIBLE)
+            addresource_image.setVisibility(View.VISIBLE)
+
+            addfavorite_button.setVisibility(View.VISIBLE)
+            addfavorite_image.setVisibility(View.VISIBLE)
+
+            enroll_image.setImageResource(R.drawable.leave)
+
+            enroll_button.text = "Leave"
+        }
+        else{
+            resource_image.setVisibility(View.GONE)
+            resource_button.setVisibility(View.GONE)
+
+            member_button.setVisibility(View.GONE)
+            member_image.setVisibility(View.GONE)
+
+            addresource_button.setVisibility(View.GONE)
+            addresource_image.setVisibility(View.GONE)
+
+            addfavorite_button.setVisibility(View.GONE)
+            addfavorite_image.setVisibility(View.GONE)
+
+            enroll_image.setImageResource(R.drawable.addmember)
+
+            enroll_button.text = "Enroll"
+        }
+
+    }
+
+
+    fun initScreen(){
+        var isMember:Boolean=false
+        val apiService = learningSpace2GetEnrolledLearningSpaces_api_call()
+        apiService.getEnrolledSpaces() {
+            if (it?.data != null) {
+                for (i in 0..(it.data.size - 1)) {
+
+                    if (it.data[i].id == learningSpaceID) {
+                        isMember=true
+                        break
+                    }
+                }
+                updateScreen(isMember)
+            }
+            else{
+                Log.d("API Call for getIsMember","has failed")
+            }
+        }
+
+    }
+
+
     fun onClickenroll(view:View) {
 
-        var isMember = false
+        isMember=false
         val apiService = learningSpace2GetEnrolledLearningSpaces_api_call()
         apiService.getEnrolledSpaces() {
             if (it?.data != null) {
@@ -85,74 +169,51 @@ class LearningSpace2Menu : AppCompatActivity() {
                         break
                     }
                 }
-            }
-        }
 
-        if (isMember) {
+                if (isMember) {
 
-            val apiService = learningSpace2Leave_api_call()
-            val userInfo = learningspace2Enroll_send_model(
-                learning_space_id = learningSpaceID
-            )
-            apiService.leaveUser(userInfo) {
-                if (it?.id != null) {
-                    resource_image.setVisibility(View.GONE)
-                    resource_button.setVisibility(View.GONE)
+                    val apiService = learningSpace2Leave_api_call()
+                    val userInfo = learningspace2Enroll_send_model(
+                        learning_space_id = learningSpaceID
+                    )
+                    apiService.leaveUser(userInfo) {
+                        if (it?.id != null) {
+                            updateScreen(false)
+                        } else { // leave did not work
+                            Log.d(
+                                "omer",
+                                it.toString() + " " + learningSpaceID.toString() + "adam leave edemiyo" + user_token
+                            )
 
-                    member_button.setVisibility(View.GONE)
-                    member_image.setVisibility(View.GONE)
+                        }
+                    }
 
-                    addresource_button.setVisibility(View.GONE)
-                    addresource_image.setVisibility(View.GONE)
-
-                    addfavorite_button.setVisibility(View.GONE)
-                    addfavorite_image.setVisibility(View.GONE)
-
-                    enroll_image.setImageResource(R.drawable.leave)
-
-                    enroll_button.text = "LEAVE"
-                } else { // leave did not work
-                    Log.d(
-                        "omer",
-                        it.toString() + " " + learningSpaceID.toString() + "adam leave edemiyo" + user_token
+                }
+                else{
+                    val apiService = learningSpace2Enroll_api_call()
+                    val userInfo = learningspace2Enroll_send_model(
+                        learning_space_id = learningSpaceID
                     )
 
-                }
-            }
+                    apiService.enrollUser(userInfo) {
+                        //Log.d("omer enroll",it.toString())
+                        if(it?.id!=null){ // success
+                            updateScreen(true)
+                        }
+                        else{ // enroll is unsucess
+                            Log.d("unsucess enroll","omer")
+                        }
 
-        }
-        else{
-            val apiService = learningSpace2Enroll_api_call()
-            val userInfo = learningspace2Enroll_send_model(
-                learning_space_id = learningSpaceID
-            )
-
-            apiService.enrollUser(userInfo) {
-                //Log.d("omer enroll",it.toString())
-                if(it?.id!=null){ // success
-                    resource_image.setVisibility(View.VISIBLE)
-                    resource_button.setVisibility(View.VISIBLE)
-
-                    member_button.setVisibility(View.VISIBLE)
-                    member_image.setVisibility(View.VISIBLE)
-
-                    addresource_button.setVisibility(View.VISIBLE)
-                    addresource_image.setVisibility(View.VISIBLE)
-
-                    addfavorite_button.setVisibility(View.VISIBLE)
-                    addfavorite_image.setVisibility(View.VISIBLE)
-
-                    enroll_image.setImageResource(R.drawable.addmember)
-
-                    enroll_button.text = "Enroll"
+                    }
 
                 }
-                else{ // enroll is unsucess
 
-                }
 
             }
+            else{
+                Log.d("API Call for getIsMember","has failed")
 
+            }
         }
 
     }
