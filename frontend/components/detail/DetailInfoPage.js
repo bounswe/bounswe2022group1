27abfddd018de43132dda3ffb9a29a5e29595ab9
huseyin.seyyid <huseyin.seyyid@artiwise.com>
2022-12-21 22:25:58 +0300

    footer deleted

--- frontend/components/detail/DetailInfoPage.js
+++ frontend/components/detail/DetailInfoPage.js
@@ -1,23 +1,21 @@
-import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import Container from "@mui/material/Container";
-import Footer from "./Footer";
-import { Typography } from "@mui/material";
-import Button from "@mui/material/Button";
-import DetailMain from "./DetailMain";
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Footer from './Footer';
+import { Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import DetailMain from './DetailMain';
 
 export default function DetailInfoPage({ space, content }) {
-  return (
-    <Container sx={{ marginTop: 12 }}>
-      <Grid container maxWidth="lg" spacing={2} columns={12}>
+	return (
+		<Container sx={{ marginTop: 12 }}>
+			<Grid container maxWidth="lg" spacing={2} columns={12}>
+				<Grid item xs={12}>
+					<DetailMain space={space} content={content} />
+				</Grid>
+			</Grid>
 
-        <Grid item xs={12}>
-          <DetailMain space={space} content={content} />
-        </Grid>
-      </Grid>
-
-      <Footer title="BUDEMI" description="a company of bogazici university" />
-    </Container>
-  );
+		</Container>
+	);
 }
