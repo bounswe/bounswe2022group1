114829da54b67d74ef3df3b9f408a9294d073ba0
mstfatay <ataymstf35@gmail.com>
2022-12-25 01:22:37 +0300

    test adding text and image annotations

--- frontend/components/content/TextContentsPage.js
+++ frontend/components/content/TextContentsPage.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState, useEffect, useRef } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
@@ -8,6 +9,9 @@ import Button from "@mui/material/Button";
 import ContentSidebar from "./ContentSidebar";
 import ContentMain from "./ContentMain";
 import Discussion from "./Discussion";
+
+import "@recogito/recogito-js/dist/recogito.min.css";
+import "@recogito/annotorious/dist/annotorious.min.css";
 
 const sidebar = {
   ownersList: [
@@ -26,8 +30,64 @@ const sidebar = {
 };
 
 export default function TextContentsPage({ content, comments }) {
+  const paraEl = useRef();
+  const imgEl = useRef();
+
+  const [reco, setReco] = useState();
+  const [anno, setAnno] = useState();
+
+  const [called, setCalled] = useState(false);
+
+  // Current drawing tool name
+  const [tool, setTool] = useState("rect");
+
+  useEffect(() => {
+    if (called) return;
+    setCalled(true);
+    import("@recogito/recogito-js").then((mod) => {
+      const Recogito = mod.Recogito;
+
+      const r = new Recogito({ content: paraEl.current });
+
+      r.setAuthInfo({
+        id: "mustafa",
+        displayName: "mustafa",
+      });
+
+      r.on("createAnnotation", (annotation) => {
+        console.log(annotation);
+      });
+
+      setReco(r);
+    });
+
+    import("@recogito/annotorious").then((mod) => {
+      const Annotorious = mod.Annotorious;
+
+      console.log(mod);
+      const a = new Annotorious({ image: imgEl.current });
+
+      a.setAuthInfo({
+        id: "mustafa",
+        displayName: "mustafa",
+      });
+    });
+  }, []);
+
+  //return <div ref={paraEl} />;
+
   return (
     <Container sx={{ marginTop: 12 }}>
+      <p ref={paraEl}>
+        This is a paragraph to be annotated. Lorem ipsum lorem lorem ipsum ipsum
+        lorem ipsum
+      </p>
+      <img
+        src="https://www.avatarsinpixels.com/Public/images/previews/minipix4.png"
+        ref={imgEl}
+        width="500"
+        height="500"
+      />
       <Grid container maxWidth="lg">
         <Typography
           component="h4"
