4e05f5726e0cd0d370a63392de560190217ec272
omerozdemir1 <omer.ozdemir1@boun.edu.tr>
2022-12-24 20:13:44 +0300

    see all notes api connection is made

---
+++ android/app/src/main/java/com/example/myapplication/model/learningSpace3_see_all_note_receive_model.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.model
+
+import com.google.gson.annotations.SerializedName
+
+data class learningSpace3_see_all_note_receive_model(
+    @SerializedName("data")
+    val data:Array<note_model>
+)
