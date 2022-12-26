bf74d7944732954771437af30b583ad0cc4b471a
Kadir Gökhan Sezer <kadirgokhann@gmail.com>
2022-12-26 07:47:36 +0300

    auth layer issues are solved

--- frontend/pages/forgot-password/index.js
+++ frontend/pages/forgot-password/index.js
@@ -4,12 +4,15 @@ import { useRouter } from "next/router";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const router = useRouter();
 
-  const handleClick = (e) => {
+  function handleSubmit(event) {
+    event.preventDefault();
     fetch("http://3.89.218.253:8000/app/change-password/", {
       method: "PUT",
+      headers: {
+        "content-type": "applications/json",
+      },
       body: JSON.stringify({
         email: email,
       }),
@@ -25,49 +28,22 @@ export default function SignUp() {
       .catch((err) => {
         console.log(err);
       });
-    e.preventDefault();
-  };
-
-  function handleSubmit(event) {
-    console.log("username: " + email);
-    console.log("password: " + password);
-    fetch("http://3.89.218.253:8000/app/login/", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        username: email,
-        password: password,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.token) {
-          localStorage.setItem("token", data.token);
-          localStorage.setItem("username", email);
-          router.push("/profile");
-          return;
-        } else {
-          alert("Failed with status code " + data.status);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    event.preventDefault();
   }
 
   return (
-    <Box display="flex" justifyContent="center">
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        height: "calc(100vh - 70px)",
+      }}
+    >
       <Box
         sx={{
           padding: 6,
-          marginTop: 15,
+          borderRadius: "16px",
           backgroundColor: "#dae7fb",
-          borderRadius: "15px",
         }}
       >
         <Typography
@@ -76,45 +52,35 @@ export default function SignUp() {
           component="h1"
           variant="h5"
         >
-          Sign Up
+          Forgot Password
+        </Typography>
+
+        <Typography sx={{ my: 1 }} textAlign="center">
+          Please enter your email address.
+          <br />
+          We will you sent link for reset password
         </Typography>
 
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <TextField
             margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
             fullWidth
             name="email"
             label="Email"
             type="email"
             id="email"
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
 
           <Button
-            type="submit"
             fullWidth
+            type="submit"
             variant="contained"
-            sx={{ mt: 3, mb: 2, borderRadius: "16px" }}
+            sx={{ mt: 3, borderRadius: "16px" }}
           >
-            Sign Up
+            Send Email
           </Button>
         </Box>
       </Box>
