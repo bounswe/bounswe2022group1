5ab0d76a574d5d8e72c7814da216bae12285acf6
Ege Onur Taga <42147612+egetaga@users.noreply.github.com>
2022-12-26 16:17:37 +0300

    Bugfixed the multiple favorites problem

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import LearningSpace, Content, Discussion, Profile
+from .models import LearningSpace, Content, Discussion, Profile, Favorite
 
 
 # Register your models here.
@@ -7,4 +7,6 @@ admin.site.register(LearningSpace)
 admin.site.register(Content)
 admin.site.register(Discussion)
 admin.site.register(Profile)
+admin.site.register(Favorite)
+
 
