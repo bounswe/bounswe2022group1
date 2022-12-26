9e9f1cfba29092d567a92ca24727b7061add015c
Kadir Gökhan Sezer <kadirgokhann@gmail.com>
2022-12-26 21:16:18 +0300

    filter button is adde

--- frontend/components/homepage/Forum.js
+++ frontend/components/homepage/Forum.js
@@ -11,6 +11,7 @@ import {
   CardMedia,
 } from "@mui/material";
 import NextLink from "next/link";
+import { useEffect, useState } from "react";
 
 function getRandomColor(index) {
   const colors = [
@@ -25,11 +26,55 @@ function getRandomColor(index) {
   return colors[index % colors.length];
 }
 
+const handleFilterTags = (forumList, newForumList, tag, setNewForumList) => {
+  let newList = [];
+  setNewForumList(forumList);
+  if (tag === "All") return;
+  forumList.map((data) => {
+    if (data.tag === tag) {
+      newList.push(data);
+    }
+  });
+  setNewForumList(newList);
+};
+
+const tagNames = ["All"];
+
 export default function Forum({ forumList, sx }) {
+  const [newForumList, setNewForumList] = useState(forumList);
+
+  const [filterTag, setFilterTag] = useState(null);
+
+  const groupBy = function (xs, key) {
+    return xs.reduce(function (rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
+
+  const forumListGroup = groupBy(forumList, ["tag"]);
+  Object.entries(forumListGroup).map(
+    (data) => !tagNames.includes(data[0]) && tagNames.push(data[0])
+  );
+
+  const handleFilter = (data) => {
+    handleFilterTags(forumList, newForumList, data, setNewForumList);
+  };
+
   return (
     <Box sx={sx}>
+      <Box m={1}>
+        {tagNames.map((data) => (
+          <Chip
+            sx={{ p: 1, mr: 1 }}
+            label={data}
+            onClick={() => handleFilter(data)}
+          />
+        ))}
+      </Box>
+
       <Grid container spacing={2} flexDirection="row">
-        {forumList?.map((data, index) => (
+        {newForumList?.map((data, index) => (
           <Grid item xs={4} key={index}>
             <Card sx={{ boxShadow: 10 }}>
               <CardMedia component="image"></CardMedia>
@@ -43,7 +88,7 @@ export default function Forum({ forumList, sx }) {
                 >
                   {data?.name}
                 </Typography>
-                <Chip color={getRandomColor(index)} label={data?.tag} />
+                <Chip size="small" label={data?.tag} />
               </CardContent>
               <CardActions>
                 <Link
