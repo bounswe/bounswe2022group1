6f54a72b5e85d7e871545ef2c7352a3fc7cdae74
Ömer Özdemir <omer.ozdemir1@boun.edu.tr>
2022-12-27 08:00:55 +0300

    Revert "Android main "

--- android/app/src/main/java/com/example/myapplication/model/patch_profile_image_send_model.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.model
-
-import com.google.gson.annotations.SerializedName
-
-data class patch_profile_image_send_model(
-    @SerializedName("image")
-    val image:String
-)
