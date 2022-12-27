5ac1c66a48ab48a59a44ee941f0252293ad1aad5
acyishere <acyazici@outlook.com>
2022-12-27 07:11:50 +0300

    navigation bar added to create profile

--- android/app/src/main/java/com/example/myapplication/view/ProfileCreateActivity.kt
+++ android/app/src/main/java/com/example/myapplication/view/ProfileCreateActivity.kt
@@ -12,17 +12,22 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Base64
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.myapplication.R
+import com.example.myapplication.model.ls_members
 import com.example.myapplication.model.patch_profile_send_model
 import com.example.myapplication.model.profile_edit_post_send_model
 import com.example.myapplication.service.patch_profile_api_call
 import com.example.myapplication.service.profile_edit_api_call
+import com.google.android.material.navigation.NavigationView
 import java.io.ByteArrayOutputStream
 
 
@@ -56,7 +61,101 @@ class ProfileCreateActivity : AppCompatActivity() {
             }
         }
 
+        navMenuHandler()
+    }
 
+    private lateinit var checkmembers: MutableList<ls_members>
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    fun goToLearningSpace2() {
+        var intent= Intent(applicationContext, LearningSpace2Menu::class.java)
+        startActivity(intent)
+    }
+
+    fun editProfileButton() {
+        var intent= Intent(applicationContext, ProfileCreateActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun editProfileButton(view: View) {
+        var intent= Intent(applicationContext, ProfileCreateActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun toChangePassword(view: View) {
+        var intent = Intent(applicationContext, ChangePasswordActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun logoffToLanding() {
+        user_token=""
+        var intent= Intent(applicationContext, LandingActivity::class.java)
+        startActivity(intent)
+    }
+    fun toProfile() {
+        var intent= Intent(applicationContext, ProfilePageActivity::class.java)
+        startActivity(intent)
+    }
+    fun goToLearningSpace1() {
+        var intent= Intent(applicationContext, LearningSpace1::class.java)
+        startActivity(intent)
+    }
+
+    fun goToHomePage() {
+        var intent= Intent(applicationContext, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun navMenuHandler() {
+        val string = findViewById<DrawerLayout>(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, string, R.string.open, R.string.close)
+
+        string.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val string2 = findViewById<NavigationView>(R.id.navView)
+
+        string2.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.profileButton -> toProfile()
+                R.id.miItem1 -> goToHomePage()
+                R.id.miItem2 -> {
+                    selectedTAG = "Art"
+                    goToLearningSpace1()
+                }
+                R.id.miItem3 -> {
+                    selectedTAG = "Science"
+                    goToLearningSpace1()
+                }
+                R.id.miItem4 -> {
+                    selectedTAG = "Math"
+                    goToLearningSpace1()
+                }
+                R.id.miItem5 -> {
+                    selectedTAG = "Technology"
+                    goToLearningSpace1()
+                }
+                R.id.miItem6 -> {
+                    selectedTAG = "Engineering"
+                    goToLearningSpace1()
+                }
+                R.id.signOut -> {
+                    logoffToLanding()
+                }
+            }
+            true
+        }
     }
 
     fun goToProfile() {
